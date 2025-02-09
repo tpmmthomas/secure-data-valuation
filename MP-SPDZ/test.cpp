40f12418886b4f451b288c9241e218f86511c4dc
tpmmthomas <good682395@yahoo.com.hk>
2025-02-10 07:19:58 +0800

    merge

---
+++ MP-SPDZ/test.cpp
@@ -0,0 +1,11 @@
+// test.cpp
+#include <string>
+int main() { std::string s; return 0; }
+
+// /usr/include/c++/11
+//  /usr/include/x86_64-linux-gnu/c++/11
+//  /usr/include/c++/11/backward
+//  /usr/lib/gcc/x86_64-linux-gnu/11/include
+
+//  /usr/bin/../lib/gcc/x86_64-linux-gnu/12/../../../../include/c++
+//  /usr/lib/llvm-14/lib/clang/14.0.0/include
